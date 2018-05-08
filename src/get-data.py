f82f0968b915923929b2dc4bdfc10b673c9859c2
Omaro <omarodafo@gmail.com>
2018-05-09 02:08:22 +0300

    ijf.judobase data is fetching and saving in csv.

--- src/get-data.py
+++ src/get-data.py
@@ -1,19 +1,20 @@
 import urllib.request, json
+import pandas as pd
 
 sayfa = "https://data.ijf.org/api/get_json?access_token=&params[action]=competition.contests&params[__ust]=&params[id_competition]={0}&params[id_weight]={1}&params[empty]=true"
-competition = 0
+competition = 999
 
 while competition < 1575:
 	competition += 1
 	flag = 0
-	print(100*"*")
+	print(100*"*", "\n")
 	while flag < 14:
 		flag += 1
 		vericek= sayfa.format(competition, flag)
 		print(vericek)
 		with urllib.request.urlopen(vericek) as url:
 			data = json.loads(url.read().decode())
-			veri = json.dumps(data, indent=2)
+			veri = data['contests']
 			
-			for contest in data['contests']:
-				print(contest['fight_no'], contest['id_fight'], contest['round_name'], contest['person_white'], contest['person_blue'], contest['ippon_b'], contest['waza_b'], contest['yuko_b'], contest['penalty_b'], contest['ippon_w'], contest['waza_w'], contest['yuko_w'], contest['penalty_w'])
+		my_df = pd.DataFrame(veri)
+		my_df.to_csv('data.csv', mode='a', index=False, header=False)
