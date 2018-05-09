360e342e322c28ac753c1d6f5d963276008f7366
Omaro <omarodafo@gmail.com>
2018-05-10 02:20:55 +0300

    competition ids reversed

--- src/get-data.py
+++ src/get-data.py
@@ -7,10 +7,10 @@ timeout = 10
 socket.setdefaulttimeout(timeout)
 
 sayfa = "https://data.ijf.org/api/get_json?access_token=&params[action]=competition.contests&params[__ust]=&params[id_competition]={0}&params[id_weight]={1}&params[empty]=true"
-competition = 999
+competition = 1575
 
-while competition < 1575:
-	competition += 1
+while competition > 999:
+	competition -= 1
 	flag = 0
 	print(100*"*", "\n")
 	while flag < 14:
