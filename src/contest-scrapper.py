e5237bb834003e1cc7b3fc1d25d28763a49823c9
omerdeger <omerdeger@pop-os.localdomain>
2019-11-26 17:36:13 +0300

    has_result competition added to csv

--- src/contest-scrapper.py
+++ src/contest-scrapper.py
@@ -1,25 +1,47 @@
 import socket
 import urllib.request, json
 import pandas as pd
+from alive_progress import alive_bar
+from datetime import datetime
 
 # timeout in seconds
 timeout = 10
 socket.setdefaulttimeout(timeout)
 
-sayfa = "https://data.ijf.org/api/get_json?access_token=&params[action]=competition.contests&params[__ust]=&params[id_competition]={0}&params[id_weight]={1}&params[empty]=true"
-competition = 1575
 
-while competition > 999:
-	competition -= 1
-	flag = 0
-	print(100*"*", "\n")
-	while flag < 14:
-		flag += 1
-		vericek= sayfa.format(competition, flag)
-		print(vericek)
-		with urllib.request.urlopen(vericek) as url:
-			data = json.loads(url.read().decode())
-			veri = data['contests']
-			
-		my_df = pd.DataFrame(veri)
-		my_df.to_csv('data.csv', mode='a', index=False, header=False)
+def get_latest_events():
+	events_url = "https://data.ijf.org/api/get_json?access_token=&params[action]=competition.get_list&params[__ust]=&params[year]=&params[month]=&params[rank_group]=&params[sort]=-1"
+
+	with urllib.request.urlopen(events_url) as url:
+		data = json.loads(url.read().decode())
+
+	finished_competition = []
+	for competition in data:
+		has_result = (competition['has_results'])
+		if (has_result != "0"):
+			finished_competition.append(competition['id_competition'])
+		
+	return finished_competition
+
+
+
+# For adding date and time to filename 
+get_datetime = datetime.now()
+get_datetime = (get_datetime.strftime('%d-%m-%Y__%H-%M'))
+
+data_url = "https://data.ijf.org/api/get_json?access_token=&params[action]=competition.contests&params[__ust]=&params[id_competition]={0}&params[id_weight]={1}&params[empty]=true"
+with alive_bar(len(get_latest_events())) as bar:
+	for competition_id in get_latest_events():
+		competition = competition_id
+		weight_id = 0
+
+		while weight_id < 14:
+			weight_id += 1
+			target_url = data_url.format(competition, weight_id)
+			with urllib.request.urlopen(target_url) as url:
+				data = json.loads(url.read().decode())
+				data_pd = data['contests']
+				
+			dataframe = pd.DataFrame(data_pd)
+			dataframe.to_csv('contests-data-{}.csv'.format(get_datetime), mode='a', index=False, header=False)
+		bar()
